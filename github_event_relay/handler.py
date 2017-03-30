026696e5544ab41400d12db09eff367295647831
Alex Ellis <alexellis2@gmail.com>
2017-03-30 23:06:54 +0100

    Update handler.py

--- github_event_relay/handler.py
+++ github_event_relay/handler.py
@@ -10,5 +10,12 @@ def get_stdin():
 
 if(__name__=="__main__"):
     stdin_values = get_stdin()
+    
+    # stdin_values includes the event from Github, you can inspect it and decide what to do.
+    # - the event could be a new subscriber (watcher), fork or a star, so you may want to break this down
+    
+    # Here we're calling into another function called "star" which exists to help us track which events were stars
+    # later on.
     res = requests.post("http://gateway:8080/function/func_star", json={})
+    
     print(res)
